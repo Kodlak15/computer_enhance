09fd4643e46b1a105ac963b303eb6a2bd8a93177
Kodlak15 <stanlcod15@protonmail.com>
2026-01-05 22:37:33 -0800

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Homework solutions for the Computer Enhance programming series by Casey Muratori";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin"];
+      perSystem = {
+        config,
+        self',
+        inputs',
+        pkgs,
+        system,
+        ...
+      }: {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+          ];
+
+          shellHook = ''
+            exec zsh -c zellij
+          '';
+        };
+      };
+    };
+}
