f23963e7c04cf072887fe498c65d697b5693caf9
Kodlak15 <stanlcod15@protonmail.com>
2026-02-07 19:17:58 -0800

    Finish first homework in cpp

--- homework/8086/src/8086.cpp
+++ homework/8086/src/8086.cpp
@@ -9,20 +9,112 @@ void print_byte_as_bits(char byte) {
     }
 }
 
+const char *decode_register(char byte, char w) {
+    switch (byte) {
+    case 0b000:
+        if (w == 0) {
+            return "al";
+        } else {
+            return "ax";
+        }
+    case 0b001:
+        if (w == 0) {
+            return "cl";
+        } else {
+            return "cx";
+        }
+    case 0b010:
+        if (w == 0) {
+            return "dl";
+        } else {
+            return "dx";
+        }
+    case 0b011:
+        if (w == 0) {
+            return "bl";
+        } else {
+            return "bx";
+        }
+    case 0b100:
+        if (w == 0) {
+            return "ah";
+        } else {
+            return "sp";
+        }
+    case 0b101:
+        if (w == 0) {
+            return "ch";
+        } else {
+            return "bp";
+        }
+    case 0b110:
+        if (w == 0) {
+            return "dh";
+        } else {
+            return "si";
+        }
+    case 0b111:
+        if (w == 0) {
+            return "bh";
+        } else {
+            return "di";
+        }
+    }
+
+    return nullptr;
+}
+
 int decode_instructions(char *path) {
-    FILE *file = fopen(path, "r");
-    if (file == nullptr) {
+    FILE *fptr = fopen(path, "r");
+    if (fptr == nullptr) {
         printf("Unable to open file at `%s`\n", path);
         return 1;
     }
 
-    char byte;
-    while ((byte = fgetc(file)) != EOF) {
-        print_byte_as_bits(byte);
-        putchar('\n');
-        // Instruction decoding here ...
+    char byte1;
+    while ((byte1 = fgetc(fptr)) != EOF) {
+        // print_byte_as_bits(byte1);
+        // putchar('\n');
+        // print_byte_as_bits(byte1 >> 2 & 0b00111111);
+        // putchar('\n');
+
+        // printf("Register to register? %s\n",
+        //        ((byte1 >> 2) & 0b00111111) == 0b100010 ? "true" : "false");
+
+        printf("bits 16\n\n");
+
+        // Register to register move
+        if (((byte1 >> 2) & 0b00111111) == 0b100010) {
+            // the d bit tells us which direction the data moves
+            char d = byte1 & 0b00000010;
+            // the w bit tells us whether we need an 8 bit or 16 bit register
+            char w = byte1 & 0b00000001;
+
+            // Get the next byte
+            char byte2 = fgetc(fptr);
+
+            // The mod field tells us what kind of move this is
+            char mod = (byte2 >> 6) & 0b00000011;
+
+            char reg = (byte2 >> 3) & 0b00000111;
+            char rm = byte2 & 0b00000111;
+
+            // Decode the register ...
+            const char *src;
+            const char *dst;
+            if (d == 0) {
+                src = decode_register(reg, w);
+                dst = decode_register(rm, w);
+            } else {
+                src = decode_register(rm, w);
+                dst = decode_register(reg, w);
+            }
+
+            // Print the decoded instruction
+            printf("mov %s, %s\n", dst, src);
+        }
     }
-    fclose(file);
+    fclose(fptr);
 
     return 0;
 }
