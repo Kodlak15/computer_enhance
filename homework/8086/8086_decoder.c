258044c558b86903e99e7a391ad826aa30d2f749
Kodlak15 <stanlcod15@protonmail.com>
2026-06-26 15:45:41 -0700

    Set up more lookup table entries

--- homework/8086/8086_decoder.c
+++ homework/8086/8086_decoder.c
@@ -3,12 +3,64 @@
 
 Instruction mov_rm_reg(FileContents file, size_t offset, size_t consumed);
 Instruction mov_imm_reg(FileContents file, size_t offset, size_t consumed);
-Instruction add_rm_reg(FileContents file, size_t offset, size_t consumed);
-Instruction add_imm_rm(FileContents file, size_t offset, size_t consumed);
-Instruction add_imm_accum(FileContents file, size_t offset, size_t consumed);
+Instruction add_sub_cmp_rm_reg(FileContents file, size_t offset, size_t consumed);
+Instruction add_sub_cmp_imm_rm(FileContents file, size_t offset, size_t consumed);
+Instruction add_sub_cmp_imm_accum(FileContents file, size_t offset, size_t consumed);
 
-typedef Instruction (*instruction_handler)(FileContents file, size_t offset, size_t consumed, const char *mnemonic);
-instruction_handler handler_table[256];
+typedef Instruction (*OperationHandler)(FileContents file, size_t offset, size_t consumed);
+OperationHandler handler_table[256] = {
+    // Add: Reg/memory with register to either
+    [0x00] = add_sub_cmp_rm_reg,
+    [0x01] = add_sub_cmp_rm_reg,
+    [0x02] = add_sub_cmp_rm_reg,
+    [0x03] = add_sub_cmp_rm_reg,
+    // Add: Immediate to accumulator
+    [0x04] = add_sub_cmp_imm_accum,
+    [0x05] = add_sub_cmp_imm_accum,
+    // Sub: Reg/memory and register to either
+    [0x28] = add_sub_cmp_rm_reg,
+    [0x29] = add_sub_cmp_rm_reg,
+    [0x2a] = add_sub_cmp_rm_reg,
+    [0x2b] = add_sub_cmp_rm_reg,
+    // Sub: Immediate to accumulator
+    [0x2c] = add_sub_cmp_imm_accum,
+    [0x2d] = add_sub_cmp_imm_accum,
+    // Cmp: Register/memory and register
+    [0x38] = add_sub_cmp_rm_reg,
+    [0x39] = add_sub_cmp_rm_reg,
+    [0x3a] = add_sub_cmp_rm_reg,
+    [0x3b] = add_sub_cmp_rm_reg,
+    // Cmp: Immediate with accumulator
+    [0x3c] = add_sub_cmp_imm_accum,
+    [0x3d] = add_sub_cmp_imm_accum,
+    // Add/Sub/Cmp: Immediate to/from/with register/memory (all have same first byte)
+    [0x80] = add_sub_cmp_imm_rm,
+    [0x81] = add_sub_cmp_imm_rm,
+    [0x82] = add_sub_cmp_imm_rm,
+    [0x83] = add_sub_cmp_imm_rm,
+    // Move: Register/memory to/from register
+    [0x88] = mov_rm_reg,
+    [0x89] = mov_rm_reg,
+    [0x8a] = mov_rm_reg,
+    [0x8b] = mov_rm_reg,
+    // Move: Immediate to register
+    [0xb0] = mov_imm_reg,
+    [0xb1] = mov_imm_reg,
+    [0xb2] = mov_imm_reg,
+    [0xb3] = mov_imm_reg,
+    [0xb4] = mov_imm_reg,
+    [0xb5] = mov_imm_reg,
+    [0xb6] = mov_imm_reg,
+    [0xb7] = mov_imm_reg,
+    [0xb8] = mov_imm_reg,
+    [0xb9] = mov_imm_reg,
+    [0xba] = mov_imm_reg,
+    [0xbb] = mov_imm_reg,
+    [0xbc] = mov_imm_reg,
+    [0xbd] = mov_imm_reg,
+    [0xbe] = mov_imm_reg,
+    [0xbf] = mov_imm_reg,
+};
 
 DecoderResult decode(FileContents file) {
     DecoderResult result = {NULL, 0};
@@ -21,7 +73,8 @@ DecoderResult decode(FileContents file) {
     size_t count = 0;
     while (offset < file.size) {
         size_t consumed = 0;
-        Instruction instruction = decode_instruction(file, offset, &consumed);
+        // Instruction instruction = decode_instruction(file, offset, &consumed);
+        Instruction instruction = handler_table[file.bytes[offset]](file, offset, consumed);
         instructions[count++] = instruction;
         offset += consumed;
     }
@@ -31,19 +84,19 @@ DecoderResult decode(FileContents file) {
     return result;
 }
 
-Instruction decode_instruction(FileContents file, size_t offset, size_t *consumed) {
-    Instruction instruction;
-
-    // Check this out, seems really nice: https://gist.github.com/tweetandcode/8aa6e9ce3eee0b19fd9ab0ba3c0085a3
-    // User basically created a lookup table for every possible first byte that called the appropriate function
-
-    uint8_t b1 = file.bytes[offset];
-    if ((b1 & 0b11111100) == 0b10001000) {
-    } else if ((b1 & 0b11111100) == 0b10000000) {
-    } else if ((b1 & 0b11000100) == 0b00000100) {
-    } else if ((b1 & 11110000) == 0b01110000) {
-    } else if ((b1 & 0b11111100) == 0b11100000) {
-    }
-
-    return instruction;
-}
+// Instruction decode_instruction(FileContents file, size_t offset, size_t *consumed) {
+//     Instruction instruction;
+//
+//     // Check this out, seems really nice: https://gist.github.com/tweetandcode/8aa6e9ce3eee0b19fd9ab0ba3c0085a3
+//     // User basically created a lookup table for every possible first byte that called the appropriate function
+//
+//     uint8_t b1 = file.bytes[offset];
+//     if ((b1 & 0b11111100) == 0b10001000) {
+//     } else if ((b1 & 0b11111100) == 0b10000000) {
+//     } else if ((b1 & 0b11000100) == 0b00000100) {
+//     } else if ((b1 & 11110000) == 0b01110000) {
+//     } else if ((b1 & 0b11111100) == 0b11100000) {
+//     }
+//
+//     return instruction;
+// }
